fa5049421a257337c7a550fa20001628d772ef2b
Marcus Lai <marcus41881653@gmail.com>
2022-01-12 01:41:13 -0800

    changed website title and icon

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 import Box from './components/box.jsx'
 import Bar from './components/bar.jsx'
